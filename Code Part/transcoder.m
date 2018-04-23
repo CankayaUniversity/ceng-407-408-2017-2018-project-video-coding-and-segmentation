9239c9e70dd55cf230ad3e66a638c1a2143214eb
bekoberk94 <bekoberk.94@gmail.com>
2018-04-24 00:52:32 +0300

    Update transcoder.m

--- Code Part/transcoder.m
+++ Code Part/transcoder.m
@@ -1,70 +1,33 @@
 
 function transcoder
 fprintf('\nContent Based Video Segmentation\n')
-%number of frames, 0 to transcode all frames
 nf = 0;
 fprintf('Transcoding all frames\n')
-%load video encoded with mpegproj.m
 load lastmov
-%load logo encoded with encodeLogo.m
-load logo
-%start timer for transcode time
 tic
-mov3 = transmpeg(mpeg,encodedLogo);
+mov3 = transmpeg(mpeg);
 fprintf('Transcode time: %s\n', sec2timestr(toc))
-%save transcoded video(mov3) with logo insertion
 save transcoder mov3
 
 %%Transcoder
-function mpeg_transcoded = transmpeg(mpeg,logo)
+function mpeg_transcoded = transmpeg(mpeg)
 
 % Frame type
 fpat = 'IPPPP'; % Custom
 
-% Loop over frames
 k = 0;
 pf = [];
 progressbar
 for i = 1:length(mpeg)
-    %OUTPUTS of 'decframe' FUNCTION
-    %f : decoded frame
-    %vectorX_matrix : x components of motion vectores
-    %vectorY_matrix : y components of motion vectors
-    
-    %INPUTS of 'decframe' FUNCTION
-    %mpeg{i} : current encoded frame
-    %pf : previous frame
-    %logo : encoded logo
-    
-    %DESCRIPTION of 'decframe' FUNCTION
-    %decframe : decodes frame, inserts logo and
-    %stores motion vectors to use later in encframe function
-    [f, vectorX_matrix, vectorY_matrix] = decframe(mpeg{i},pf,logo);
-    %print current decoded frame number 
+
+    [f, vectorX_matrix, vectorY_matrix] = decframe(mpeg{i},pf);
     fprintf('Frame %i decoded\n', i)
-    % Get frame type
     k = k + 1;
     if k > length(fpat)
         k = 1;
     end
     ftype = fpat(k);
 
-    % Encode frame
-    %OUTPUTS of 'encframe' FUNCTION
-    %mpeg_transcoded{i} : encoded frame
-    %pf : previous frame
-    
-    %INPUTS of 'encframe' FUNCTION
-    %f :current decoded frame
-    %ftype : frame type I or P
-    %pf : previous decoded frame
-    %vectorX_matrix : x components of motion vectores
-    %vectorY_matrix : y components of motion vectors
-    
-    %DESCRIPTION of 'encframe' FUNCTION
-    %encode frame : encodes the decoded current frame and instead of
-    %recalculating motion information uses stored motion information by
-    %decframe
     [mpeg_transcoded{i},pf] = encframe(f,ftype,pf, vectorX_matrix, vectorY_matrix);
     % Cache previous frame
      totalmv(i)=calculatetotalmv(ftype,vectorX_matrix,vectorY_matrix);
@@ -101,15 +64,14 @@ end
 fprintf('hello');
 
 %%
-function [ fr, vectorX_matrix, vectorY_matrix ] = decframe( mpeg, pf, logo )
+function [ fr, vectorX_matrix, vectorY_matrix ] = decframe( mpeg, pf )
 
-%Calculate the size of frame in pixel
 mbsize = size(mpeg);%get the size of encoded frame
 M = 16 * mbsize(1); %represents the number of pixels of a column
 N = 16 * mbsize(2); %represetnts the number of pixels of a row
 counter = 1;%initialize counter for logo macroblocks to be decoded
 
-fr = zeros(M,N,3);%initialize frame
+fr = zeros(M,N,3);
 % Loop over macroblocks
 for m = 1:mbsize(1)
     for n = 1:mbsize(2)
@@ -130,25 +92,7 @@ for m = 1:mbsize(1)
         %DESCRIPTION of 'decmacroblock' FUNCTION
         %decmacroblock : decodes macroblock without applying IDCT
         if (m >= (mbsize(1) - 1) &&  n >=  (mbsize(2) - 1)  )%insert logo to the 4 macroblocks located on the lower right corner of the frame
-           if (counter == 1)
-               fr(x,y,:) = decmacroblock(logo(1,1),pf,1:16,1:16);%first logo macroblock to be inserted
-           elseif (counter == 2)
-               fr(x,y,:) = decmacroblock(logo(1,2),pf,17:32, 1:16);%second logo macroblock to be inserted
-           elseif (counter == 3)
-               fr(x,y,:) = decmacroblock(logo(2,1),pf,1:16, 17:32);%third logo macroblock to be inserted
-           elseif(counter == 4)
-               fr(x,y,:) = decmacroblock(logo(2,2),pf,17:32, 17:32);%forth logo macroblock to be inserted
-           end
-           counter = counter + 1;
-        else
-        % motion vectors of blocks around the logo is set to 0 (including logo)
-        % to decrease the rate of drift problem
-        % motion vectors of logo inserted macroblocks are also set to 0 due
-        % to logo is in a fixed position
-            if(m >= (mbsize(1) - 2) &&  n >=  (mbsize(2) - 2)  ) 
-                mpeg(m,n).mvx = 0;
-                mpeg(m,n).mvy = 0;
-            end
+          
         %rest of the blocks are decoded using current encoded frame 
         fr(x,y,:) = decmacroblock(mpeg(m,n),pf,x,y);
         vectorX_matrix(m,n) = mpeg(m,n).mvx;%store x component of the motion vectors of the current macroblock
@@ -406,7 +350,7 @@ for k= 1:1:18
                 for z=1:6
                     abc=x(i,j,z); %% deneme
                     if(x(i,j,z)==0)
-                        count=count+1; %% kaç tane 0 var
+                        count=count+1; %% kaÃ§ tane 0 var
                     end
                     % y=y+abs(abc);
                    t=t+abc;
